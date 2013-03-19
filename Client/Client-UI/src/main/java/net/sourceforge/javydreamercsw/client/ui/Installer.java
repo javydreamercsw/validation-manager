4293fc284b4f66471c09fc63fc6d97e9071dc361
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2013-03-19 10:12:03 -0500

    Register MySQL driver and create default connection if none present.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/Installer.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/Installer.java
@@ -3,6 +3,7 @@ package net.sourceforge.javydreamercsw.client.ui;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.netbeans.api.db.explorer.ConnectionManager;
+import org.netbeans.api.db.explorer.DatabaseConnection;
 import org.netbeans.api.db.explorer.DatabaseException;
 import org.netbeans.api.db.explorer.JDBCDriver;
 import org.netbeans.api.db.explorer.JDBCDriverManager;
@@ -10,17 +11,19 @@ import org.openide.modules.ModuleInstall;
 import org.openide.util.Exceptions;
 
 public class Installer extends ModuleInstall {
-    
+
+    private final String mysqlDriverClass = "com.mysql.jdbc.Driver";
+
     @Override
     public void restored() {
         //Check drivers
-        if(JDBCDriverManager.getDefault().getDrivers("com.mysql.jdbc.Driver").length==0){
+        if (JDBCDriverManager.getDefault().getDrivers(mysqlDriverClass).length == 0) {
             try {
                 JDBCDriverManager.getDefault().addDriver(
-                        JDBCDriver.create("mysql","MySQL",
-                        "com.mysql.jdbc.Driver",
+                        JDBCDriver.create("mysql", "MySQL",
+                        mysqlDriverClass,
                         new URL[]{new URL(
-                        "jdbc:mysql://localhost:3306/dbname")}));
+                    "nbinst:/modules/ext/mysql/mysql-connector-java-5.1.23.jar")}));
             } catch (DatabaseException ex) {
                 Exceptions.printStackTrace(ex);
             } catch (MalformedURLException ex) {
@@ -28,9 +31,19 @@ public class Installer extends ModuleInstall {
             }
         }
         //Check defined database location
-        if(ConnectionManager.getDefault().getConnections().length==0){
+        if (ConnectionManager.getDefault().getConnections().length == 0) {
             //None there, create the default
-//            DatabaseConnection.
+            if (JDBCDriverManager.getDefault().getDrivers(mysqlDriverClass).length > 0) {
+                try {
+                    ConnectionManager.getDefault().addConnection(DatabaseConnection.create(
+                            JDBCDriverManager.getDefault().getDrivers(mysqlDriverClass)[0],
+                            "jdbc:mysql://localhost:3306",
+                            "user", "validation_manager", "password", false,
+                            "Default Validation Manager"));
+                } catch (DatabaseException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
         }
     }
 }
