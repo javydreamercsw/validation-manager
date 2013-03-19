e77574ada6a52b2f37d0846c103e244586128775
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-03-19 13:20:25 -0500

    Basic Database interaction.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/ProjectChildFactory.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/ProjectChildFactory.java
@@ -2,24 +2,47 @@ package net.sourceforge.javydreamercsw.client.ui.nodes;
 
 import com.validation.manager.core.DataBaseManager;
 import com.validation.manager.core.db.Project;
+import java.beans.IntrospectionException;
 import java.util.Iterator;
 import java.util.List;
+import net.sourceforge.javydreamercsw.client.ui.DataBaseTool;
 import org.openide.nodes.ChildFactory;
+import org.openide.nodes.Node;
+import org.openide.util.Exceptions;
 
 /**
  *
  * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
  */
-public class ProjectChildFactory extends ChildFactory<Project>{
+public class ProjectChildFactory extends ChildFactory<Project> {
 
     @Override
     protected boolean createKeys(List<Project> list) {
+        //Connect to database
+        DataBaseTool.connect();
         list.clear();
         List<Object> projects = DataBaseManager.createdQuery(
-                "select p from Project p where parentProjectId=null");
+                "select p from Project p");// where p.parentProjectId is null
         for (Iterator<Object> it = projects.iterator(); it.hasNext();) {
             list.add((Project) it.next());
         }
         return true;
+    }
+
+    @Override
+    protected Node[] createNodesForKey(Project key) {
+        //TODO: show sub projects 
+        //return super.createNodesForKey(key); //To change body of generated methods, choose Tools | Templates.
+        return new Node[]{};
+    }
+
+    @Override
+    protected Node createNodeForKey(Project key) {
+        try {
+            return new ProjectNode(key);
+        } catch (IntrospectionException ex) {
+            Exceptions.printStackTrace(ex);
+            return null;
+        }
     }
 }
