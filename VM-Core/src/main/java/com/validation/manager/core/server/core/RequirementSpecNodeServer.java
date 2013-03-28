5833a5943757a5f4e875daeabe1285511b214915
jortiz00 <jortiz00@localhost>
2013-03-28 15:46:30 -0500

    Remove comments.

--- VM-Core/src/main/java/com/validation/manager/core/server/core/RequirementSpecNodeServer.java
+++ VM-Core/src/main/java/com/validation/manager/core/server/core/RequirementSpecNodeServer.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.validation.manager.core.server.core;
 
 import com.validation.manager.core.DataBaseManager;
