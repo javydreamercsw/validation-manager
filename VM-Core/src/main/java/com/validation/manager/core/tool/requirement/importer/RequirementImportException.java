d52289e27e95d8908ded4f5fc2c5d6f89034bbbe
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-03-29 15:32:09 -0500

    Translate message if a resource bundle string is used.

--- VM-Core/src/main/java/com/validation/manager/core/tool/requirement/importer/RequirementImportException.java
+++ VM-Core/src/main/java/com/validation/manager/core/tool/requirement/importer/RequirementImportException.java
@@ -1,12 +1,19 @@
 package com.validation.manager.core.tool.requirement.importer;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  *
  * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
  */
 public class RequirementImportException extends Exception {
 
+    private static ResourceBundle rb =
+            ResourceBundle.getBundle(
+            "com.validation.manager.resources.VMMessages", Locale.getDefault());
+
     public RequirementImportException(String message) {
-        super(message);
+        super(rb.containsKey(message) ? rb.getString(message) : message);
     }
 }
