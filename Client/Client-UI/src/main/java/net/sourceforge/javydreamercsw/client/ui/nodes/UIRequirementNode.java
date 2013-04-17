9b68b0efd361c1e646d9e25f7e1b262254e35bb4
jortiz00 <javydreamercsw@users.noreply.github.com>
2013-04-17 14:30:38 -0500

    Enable linking requirements.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/UIRequirementNode.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/UIRequirementNode.java
@@ -2,6 +2,11 @@ package net.sourceforge.javydreamercsw.client.ui.nodes;
 
 import com.validation.manager.core.db.Requirement;
 import java.beans.IntrospectionException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.Action;
+import net.sourceforge.javydreamercsw.client.ui.nodes.actions.EditRequirementAction;
 import org.openide.util.lookup.InstanceContent;
 
 /**
@@ -19,5 +24,13 @@ public class UIRequirementNode extends AbstractRefreshableBeanNode {
     @Override
     public String getName() {
         return getLookup().lookup(Requirement.class).getUniqueId();
+    }
+    
+    @Override
+    public Action[] getActions(boolean b) {
+        List<Action> actions = new ArrayList<Action>();
+        actions.addAll(Arrays.asList(super.getActions(b)));
+        actions.add(new EditRequirementAction());
+        return actions.toArray(new Action[actions.size()]);
     }
 }
