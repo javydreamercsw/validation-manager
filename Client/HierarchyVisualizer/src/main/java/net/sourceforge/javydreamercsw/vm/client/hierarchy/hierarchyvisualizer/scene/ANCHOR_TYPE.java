9a55e00bb56143005094c97cac9c5cf754c76e56
jortiz00 <javydreamercsw@users.noreply.github.com>
2013-05-09 14:10:22 -0500

    Shows selected requirement.

--- Client/HierarchyVisualizer/src/main/java/net/sourceforge/javydreamercsw/vm/client/hierarchy/hierarchyvisualizer/scene/ANCHOR_TYPE.java
+++ Client/HierarchyVisualizer/src/main/java/net/sourceforge/javydreamercsw/vm/client/hierarchy/hierarchyvisualizer/scene/ANCHOR_TYPE.java
@@ -1,11 +1,12 @@
 package net.sourceforge.javydreamercsw.vm.client.hierarchy.hierarchyvisualizer.scene;
 
-import com.validation.manager.core.db.Requirement;
-
 /**
  *
  * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
  */
-public class RequirementNode extends Requirement{
-    
+public enum ANCHOR_TYPE {
+    CENTER,
+    CIRCULAR,
+    DIRECTIONAL,
+    RECTANGULAR
 }
