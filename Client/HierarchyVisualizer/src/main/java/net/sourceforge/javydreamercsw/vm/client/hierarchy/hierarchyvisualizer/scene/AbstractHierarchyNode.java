bdd656184ba7406c867f2c05d751b256f863f535
jortiz00 <jortiz00@localhost>
2013-05-10 09:59:20 -0500

    1) Correct placement of requirements along with correct direction arrows. 2) New icons used.

--- Client/HierarchyVisualizer/src/main/java/net/sourceforge/javydreamercsw/vm/client/hierarchy/hierarchyvisualizer/scene/AbstractHierarchyNode.java
+++ Client/HierarchyVisualizer/src/main/java/net/sourceforge/javydreamercsw/vm/client/hierarchy/hierarchyvisualizer/scene/AbstractHierarchyNode.java
@@ -71,7 +71,7 @@ public abstract class AbstractHierarchyNode extends IconNodeWidget {
         setImage(getCurrentImage());
         setLabel(getCurrentLabel());
         if (getPreferredLocation() == null) {
-            setPreferredLocation(new Point(10, 20));
+            setPreferredLocation(new Point(300, 20));
         }
     }
 
@@ -92,4 +92,22 @@ public abstract class AbstractHierarchyNode extends IconNodeWidget {
             return new ImageIcon(imageURL, description);
         }
     }
+//     private static BufferedImage resizeImage(BufferedImage originalImage, 
+    //int type, final int IMG_WIDTH, final int IMG_HEIGHT){
+// 
+//	BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+//	Graphics2D g = resizedImage.createGraphics();
+//	g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+//	g.dispose();	
+//	g.setComposite(AlphaComposite.Src);
+// 
+//	g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+//	RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+//	g.setRenderingHint(RenderingHints.KEY_RENDERING,
+//	RenderingHints.VALUE_RENDER_QUALITY);
+//	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+//	RenderingHints.VALUE_ANTIALIAS_ON);
+// 
+//	return resizedImage;
+//    }	
 }
