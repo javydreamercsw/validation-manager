831b37600ba3d4e28d736ada20fe362fb1c5f1e6
jortiz00 <jortiz00@localhost>
2013-05-29 16:16:03 -0500

    If step list is null, default to step 1.

--- Client/UserInterface/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/actions/EditTestStepDialog.java
+++ Client/UserInterface/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/actions/EditTestStepDialog.java
@@ -59,7 +59,7 @@ public class EditTestStepDialog extends javax.swing.JDialog {
         requirements.setCellRenderer(new ListCellRenderer() {
             @Override
             public Component getListCellRendererComponent(JList list,
-                    Object value, int index, boolean isSelected, 
+                    Object value, int index, boolean isSelected,
                     boolean cellHasFocus) {
 
                 return new JLabel(
@@ -248,7 +248,8 @@ public class EditTestStepDialog extends javax.swing.JDialog {
             } else {
                 tc = Utilities.actionsGlobalContext().lookup(TestCase.class);
                 TestCaseServer tcs = new TestCaseServer(tc.getTestCasePK());
-                ss = new StepServer(tc, tcs.getStepList().size() + 1, //Add at the end by default
+                ss = new StepServer(tc, tcs.getStepList() == null ? 1
+                        : tcs.getStepList().size() + 1, //Add at the end by default
                         text.getText().trim());
             }
             try {
