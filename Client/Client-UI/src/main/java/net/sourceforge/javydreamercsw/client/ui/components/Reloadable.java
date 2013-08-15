fe19fc6518b838920e6bcb307b59895dcfad9338
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-08-15 08:14:44 -0500

    Upgraded edit requirement to a Top Component.

---
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/Reloadable.java
@@ -0,0 +1,15 @@
+package net.sourceforge.javydreamercsw.client.ui.components;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public interface Reloadable {
+
+    /**
+     * Reloads.
+     *
+     * @throws Exception if an error happens.
+     */
+    public void reload() throws Exception;
+}
