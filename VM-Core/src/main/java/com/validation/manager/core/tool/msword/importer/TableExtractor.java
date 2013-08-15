237ae32a51971f9a4f6b2111db143553419242b3
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-08-15 15:29:24 -0500

    Support to extract Tables from a .docx file.

---
+++ VM-Core/src/main/java/com/validation/manager/core/tool/msword/importer/TableExtractor.java
@@ -0,0 +1,73 @@
+package com.validation.manager.core.tool.msword.importer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public class TableExtractor {
+
+    private final File source;
+
+    public TableExtractor(File source) {
+        this.source = source;
+        assert source.getAbsolutePath().endsWith(".docx") :
+                "File not supported. Only .docx files are supported.";
+    }
+
+    public List<XWPFTable> extractTables() throws FileNotFoundException, IOException {
+        InputStream fis = new FileInputStream(source);
+        List<XWPFTable> tables = new ArrayList<XWPFTable>();
+        XWPFDocument doc = new XWPFDocument(fis);
+        tables.addAll(doc.getTables());
+        return tables;
+    }
+
+    public static void main(String[] args) {
+        JFileChooser fc = new JFileChooser();
+        int returnVal = fc.showOpenDialog(new JFrame());
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            TableExtractor te = new TableExtractor(file);
+            try {
+                List<XWPFTable> tables = te.extractTables();
+                int i = 1;
+                for (XWPFTable table : tables) {
+                    System.out.println("Table: " + i);
+                    for (int rowIndex = 0; rowIndex < table.getNumberOfRows(); rowIndex++) {
+                        XWPFTableRow row = table.getRow(rowIndex);
+                        System.out.println("Row: " + rowIndex);
+                        for (int colIndex = 0; colIndex < row.getTableCells().size(); colIndex++) {
+                            System.out.println("Column: " + colIndex);
+                            XWPFTableCell cell = row.getCell(colIndex);
+                            for (XWPFParagraph p : cell.getParagraphs()) {
+                                System.out.println(p.getText());
+                            }
+                        }
+                    }
+                    i++;
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(TableExtractor.class.getName()).log(Level.SEVERE, null, ex);
+                System.exit(1);
+            }
+        }
+        System.exit(0);
+    }
+}
---
+++ VM-Core/src/test/java/com/validation/manager/core/tool/msword/importer/TableExtractorTest.java
@@ -0,0 +1,50 @@
+package com.validation.manager.core.tool.msword.importer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public class TableExtractorTest {
+
+    /**
+     * Test of extractTables method, of class TableExtractor.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testExtractTables() throws Exception {
+        System.out.println("extractTables");
+        String name = TableExtractorTest.class.getCanonicalName();
+        name = name.substring(0, name.lastIndexOf("."));
+        name = name.replace(".", System.getProperty("file.separator"));
+        File file = new File(System.getProperty("user.dir")
+                + System.getProperty("file.separator") + "src"
+                + System.getProperty("file.separator") + "test"
+                + System.getProperty("file.separator") + "java"
+                + System.getProperty("file.separator")
+                + name
+                + System.getProperty("file.separator") + "Tables.docx");
+        assert file.exists();
+        extractFromFile(file);
+    }
+
+    private void extractFromFile(File file) {
+        TableExtractor te = new TableExtractor(file);
+        try {
+            List<XWPFTable> tables = te.extractTables();
+            assertEquals(2, tables.size());
+        } catch (IOException ex) {
+            Logger.getLogger(TableExtractor.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+}
