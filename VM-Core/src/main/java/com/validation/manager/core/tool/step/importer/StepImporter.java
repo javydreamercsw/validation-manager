8f6142ccf2dc52003e3b74bbcc16c6f4af432dee
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-08-15 13:05:32 -0500

    Remove unused import.

--- VM-Core/src/main/java/com/validation/manager/core/tool/step/importer/StepImporter.java
+++ VM-Core/src/main/java/com/validation/manager/core/tool/step/importer/StepImporter.java
@@ -19,7 +19,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
