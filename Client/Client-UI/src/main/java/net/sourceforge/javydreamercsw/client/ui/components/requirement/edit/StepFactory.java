c2ff5f6a3e3a68e14717bf9800ea8f733e29cec6
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-08-20 16:41:27 -0500

    1) Moved POI libraries to their own Wrapper Module to allow re-use. 2) Some refactoring.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/requirement/edit/StepFactory.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/requirement/edit/StepFactory.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package net.sourceforge.javydreamercsw.client.ui.components;
+package net.sourceforge.javydreamercsw.client.ui.components.requirement.edit;
 
 import com.validation.manager.core.db.Step;
 import java.util.ArrayList;
