763e1615a2e2b1edf4c98828bf212d563b0901cc
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-08-21 10:51:38 -0500

    Initial commit of the Test Importer window

---
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/actions/ImportTestAction.java
@@ -0,0 +1,27 @@
+package net.sourceforge.javydreamercsw.client.ui.nodes.actions;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import net.sourceforge.javydreamercsw.client.ui.components.test.importer.TestImportTopComponent;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public class ImportTestAction extends AbstractAction {
+
+    private final TestImportTopComponent component = new TestImportTopComponent();
+
+    public ImportTestAction() {
+        super("Import Test",
+                new ImageIcon("com/validation/manager/resources/icons/Signage/Add Square.png"));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        /* Create and display the dialog */
+        component.open();
+        component.requestActive();
+    }
+}
