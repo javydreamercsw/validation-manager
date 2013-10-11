750756f425a8b649dbd2b8c092bcfd6de311a9e8
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2013-10-11 15:31:53 -0500

    1) Move Table Extractor to a more suitable package. 2) Add excel import tests.

--- VM-Core/src/main/java/com/validation/manager/core/api/report/ReportChooserInterface.java
+++ VM-Core/src/main/java/com/validation/manager/core/api/report/ReportChooserInterface.java
@@ -1,5 +1,7 @@
 package com.validation.manager.core.api.report;
 
+import java.awt.Component;
+
 /**
  *
  * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
@@ -12,4 +14,18 @@ public interface ReportChooserInterface {
      * @param visible
      */
     public void setVisible(boolean visible);
+
+    /**
+     * Set it relative to a component.
+     *
+     * @param c
+     */
+    public void setLocationRelativeTo(Component c);
+
+    /**
+     * Check if the dialog is visible.
+     *
+     * @return true if visible.
+     */
+    public boolean isVisible();
 }
