62581071f3898392e6af8d104d7e338aac2355f6
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-12-10 14:04:30 -0600

    Enabling resize code.

--- Client/HierarchyVisualizer/src/main/java/net/sourceforge/javydreamercsw/vm/client/hierarchy/hierarchyvisualizer/scene/AbstractHierarchyNode.java
+++ Client/HierarchyVisualizer/src/main/java/net/sourceforge/javydreamercsw/vm/client/hierarchy/hierarchyvisualizer/scene/AbstractHierarchyNode.java
@@ -1,7 +1,11 @@
 package net.sourceforge.javydreamercsw.vm.client.hierarchy.hierarchyvisualizer.scene;
 
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
@@ -92,22 +96,23 @@ public abstract class AbstractHierarchyNode extends IconNodeWidget {
             return new ImageIcon(imageURL, description);
         }
     }
-//     private static BufferedImage resizeImage(BufferedImage originalImage, 
-    //int type, final int IMG_WIDTH, final int IMG_HEIGHT){
-// 
-//	BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
-//	Graphics2D g = resizedImage.createGraphics();
-//	g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
-//	g.dispose();	
-//	g.setComposite(AlphaComposite.Src);
-// 
-//	g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-//	RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-//	g.setRenderingHint(RenderingHints.KEY_RENDERING,
-//	RenderingHints.VALUE_RENDER_QUALITY);
-//	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-//	RenderingHints.VALUE_ANTIALIAS_ON);
-// 
-//	return resizedImage;
-//    }	
+
+    protected static BufferedImage resizeImage(BufferedImage originalImage,
+            int type, final int IMG_WIDTH, final int IMG_HEIGHT) {
+
+        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+        g.dispose();
+        g.setComposite(AlphaComposite.Src);
+
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        return resizedImage;
+    }
 }
