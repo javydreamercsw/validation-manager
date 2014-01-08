4239d5e07dd215c5f3853719bb124f96298227eb
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2014-01-08 11:12:49 -0600

    Back up and working.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/project/stat/ProjectStatsTopComponent.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/project/stat/ProjectStatsTopComponent.java
@@ -1,171 +1,171 @@
 package net.sourceforge.javydreamercsw.client.ui.components.project.stat;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.text.DecimalFormat;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.Chart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-import javax.swing.table.DefaultTableCellRenderer;
-import org.netbeans.api.settings.ConvertAsProperties;
-import org.openide.awt.ActionID;
-import org.openide.awt.ActionReference;
+//import java.awt.BorderLayout;
+//import java.awt.Color;
+//import java.awt.Component;
+//import java.awt.Dimension;
+//import java.text.DecimalFormat;
+//import javafx.application.Platform;
+//import javafx.collections.FXCollections;
+//import javafx.embed.swing.JFXPanel;
+//import javafx.scene.Scene;
+//import javafx.scene.chart.BarChart;
+//import javafx.scene.chart.CategoryAxis;
+//import javafx.scene.chart.Chart;
+//import javafx.scene.chart.NumberAxis;
+//import javafx.scene.chart.XYChart;
+//import javax.swing.JLabel;
+//import javax.swing.JPanel;
+//import javax.swing.JScrollPane;
+//import javax.swing.JTable;
+//import javax.swing.event.TableModelEvent;
+//import javax.swing.event.TableModelListener;
+//import javax.swing.table.DefaultTableCellRenderer;
+//import org.netbeans.api.settings.ConvertAsProperties;
+//import org.openide.awt.ActionID;
+//import org.openide.awt.ActionReference;
 import org.openide.windows.TopComponent;
-import org.openide.util.NbBundle.Messages;
+//import org.openide.util.NbBundle.Messages;
 
 /**
  * Top component which displays something.
  */
-@ConvertAsProperties(
-        dtd = "-//net.sourceforge.javydreamercsw.client.ui.components.project.stat//ProjectStats//EN",
-        autostore = false
-)
-@TopComponent.Description(
-        preferredID = "ProjectStatsTopComponent",
-        //iconBase="SET/PATH/TO/ICON/HERE", 
-        persistenceType = TopComponent.PERSISTENCE_NEVER
-)
-@TopComponent.Registration(mode = "editor", openAtStartup = false)
-@ActionID(category = "Window",
-        id = "net.sourceforge.javydreamercsw.client.ui.components.project.stat.ProjectStatsTopComponent")
-@ActionReference(path = "Menu/Window" /*, position = 333 */)
-@TopComponent.OpenActionRegistration(
-        displayName = "#CTL_ProjectStatsAction",
-        preferredID = "ProjectStatsTopComponent"
-)
-@Messages({
-    "CTL_ProjectStatsAction=ProjectStats",
-    "CTL_ProjectStatsTopComponent=ProjectStats Window",
-    "HINT_ProjectStatsTopComponent=This is a ProjectStats window"
-})
+//@ConvertAsProperties(
+//        dtd = "-//net.sourceforge.javydreamercsw.client.ui.components.project.stat//ProjectStats//EN",
+//        autostore = false
+//)
+//@TopComponent.Description(
+//        preferredID = "ProjectStatsTopComponent",
+//        //iconBase="SET/PATH/TO/ICON/HERE", 
+//        persistenceType = TopComponent.PERSISTENCE_NEVER
+//)
+//@TopComponent.Registration(mode = "editor", openAtStartup = false)
+//@ActionID(category = "Window",
+//        id = "net.sourceforge.javydreamercsw.client.ui.components.project.stat.ProjectStatsTopComponent")
+//@ActionReference(path = "Menu/Window" /*, position = 333 */)
+//@TopComponent.OpenActionRegistration(
+//        displayName = "#CTL_ProjectStatsAction",
+//        preferredID = "ProjectStatsTopComponent"
+//)
+//@Messages({
+//    "CTL_ProjectStatsAction=ProjectStats",
+//    "CTL_ProjectStatsTopComponent=ProjectStats Window",
+//    "HINT_ProjectStatsTopComponent=This is a ProjectStats window"
+//})
 public final class ProjectStatsTopComponent extends TopComponent {
-
-    private static final int PANEL_WIDTH_INT = 675;
-    private static final int PANEL_HEIGHT_INT = 400;
-    private static final int TABLE_PANEL_HEIGHT_INT = 100;
-    private static JFXPanel chartFxPanel;
-    private static SampleTableModel tableModel;
-    private Chart chart;
-
-    public ProjectStatsTopComponent() {
-        initComponents();
-        setName(Bundle.CTL_ProjectStatsTopComponent());
-        setToolTipText(Bundle.HINT_ProjectStatsTopComponent());
-        setLayout(new BorderLayout());
-        //Disable JavaFX for now
-        init();
-    }
-
-    private void createScene() {
-        chart = createBarChart();
-        chartFxPanel.setScene(new Scene(chart));
-    }
-
-    public void init() {
-        tableModel = new SampleTableModel();
-        // create javafx panel for charts
-        chartFxPanel = new JFXPanel();
-        chartFxPanel.setPreferredSize(new Dimension(PANEL_WIDTH_INT, PANEL_HEIGHT_INT));
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-
-        //JTable
-        JTable table = new JTable(tableModel);
-        table.setAutoCreateRowSorter(true);
-        table.setGridColor(Color.DARK_GRAY);
-        DecimalFormatRenderer renderer = new DecimalFormatRenderer();
-        renderer.setHorizontalAlignment(JLabel.RIGHT);
-        for (int i = 0; i < table.getColumnCount(); i++) {
-            table.getColumnModel().getColumn(i).setCellRenderer(renderer);
-        }
-
-        JScrollPane tablePanel = new JScrollPane(table);
-        tablePanel.setPreferredSize(new Dimension(PANEL_WIDTH_INT, TABLE_PANEL_HEIGHT_INT));
-
-        JPanel chartTablePanel = new JPanel();
-        chartTablePanel.setLayout(new BorderLayout());
-        chartTablePanel.add(chartFxPanel, BorderLayout.CENTER);
-
-        panel.add(chartTablePanel, BorderLayout.CENTER);
-        panel.add(tablePanel, BorderLayout.SOUTH);
-
-        add(panel, BorderLayout.CENTER);
-
-        // create JavaFX scene
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                createScene();
-            }
-        });
-    }
-
-    private BarChart createBarChart() {
-        CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setCategories(FXCollections.<String>observableArrayList(tableModel.getColumnNames()));
-        xAxis.setLabel("Year");
-
-        double tickUnit = tableModel.getTickUnit();
-
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setTickUnit(tickUnit);
-        yAxis.setLabel("Units Sold");
-
-        final BarChart tempChart = new BarChart(xAxis, yAxis, tableModel.getBarChartData());
-        tableModel.addTableModelListener(new TableModelListener() {
-
-            @Override
-            public void tableChanged(TableModelEvent e) {
-                if (e.getType() == TableModelEvent.UPDATE) {
-                    final int row = e.getFirstRow();
-                    final int column = e.getColumn();
-                    final Object value = ((SampleTableModel) e.getSource()).getValueAt(row, column);
-
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            XYChart.Series<String, Number> s = (XYChart.Series<String, Number>) tempChart.getData().get(row);
-                            BarChart.Data data = s.getData().get(column);
-                            data.setYValue(value);
-                        }
-                    });
-                }
-            }
-        });
-        return tempChart;
-    }
-
-    private static class DecimalFormatRenderer extends DefaultTableCellRenderer {
-
-        private static final DecimalFormat formatter = new DecimalFormat("#.0");
-
-        @Override
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-            value = formatter.format((Number) value);
-            return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        }
-    }
-
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
+//
+//    private static final int PANEL_WIDTH_INT = 675;
+//    private static final int PANEL_HEIGHT_INT = 400;
+//    private static final int TABLE_PANEL_HEIGHT_INT = 100;
+//    private static JFXPanel chartFxPanel;
+//    private static SampleTableModel tableModel;
+//    private Chart chart;
+//
+//    public ProjectStatsTopComponent() {
+//        initComponents();
+//        setName(Bundle.CTL_ProjectStatsTopComponent());
+//        setToolTipText(Bundle.HINT_ProjectStatsTopComponent());
+//        setLayout(new BorderLayout());
+//        //Disable JavaFX for now
+//        init();
+//    }
+//
+//    private void createScene() {
+//        chart = createBarChart();
+//        chartFxPanel.setScene(new Scene(chart));
+//    }
+//
+//    public void init() {
+//        tableModel = new SampleTableModel();
+//        // create javafx panel for charts
+//        chartFxPanel = new JFXPanel();
+//        chartFxPanel.setPreferredSize(new Dimension(PANEL_WIDTH_INT, PANEL_HEIGHT_INT));
+//
+//        JPanel panel = new JPanel();
+//        panel.setLayout(new BorderLayout());
+//
+//        //JTable
+//        JTable table = new JTable(tableModel);
+//        table.setAutoCreateRowSorter(true);
+//        table.setGridColor(Color.DARK_GRAY);
+//        DecimalFormatRenderer renderer = new DecimalFormatRenderer();
+//        renderer.setHorizontalAlignment(JLabel.RIGHT);
+//        for (int i = 0; i < table.getColumnCount(); i++) {
+//            table.getColumnModel().getColumn(i).setCellRenderer(renderer);
+//        }
+//
+//        JScrollPane tablePanel = new JScrollPane(table);
+//        tablePanel.setPreferredSize(new Dimension(PANEL_WIDTH_INT, TABLE_PANEL_HEIGHT_INT));
+//
+//        JPanel chartTablePanel = new JPanel();
+//        chartTablePanel.setLayout(new BorderLayout());
+//        chartTablePanel.add(chartFxPanel, BorderLayout.CENTER);
+//
+//        panel.add(chartTablePanel, BorderLayout.CENTER);
+//        panel.add(tablePanel, BorderLayout.SOUTH);
+//
+//        add(panel, BorderLayout.CENTER);
+//
+//        // create JavaFX scene
+//        Platform.runLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                createScene();
+//            }
+//        });
+//    }
+//
+//    private BarChart createBarChart() {
+//        CategoryAxis xAxis = new CategoryAxis();
+//        xAxis.setCategories(FXCollections.<String>observableArrayList(tableModel.getColumnNames()));
+//        xAxis.setLabel("Year");
+//
+//        double tickUnit = tableModel.getTickUnit();
+//
+//        NumberAxis yAxis = new NumberAxis();
+//        yAxis.setTickUnit(tickUnit);
+//        yAxis.setLabel("Units Sold");
+//
+//        final BarChart tempChart = new BarChart(xAxis, yAxis, tableModel.getBarChartData());
+//        tableModel.addTableModelListener(new TableModelListener() {
+//
+//            @Override
+//            public void tableChanged(TableModelEvent e) {
+//                if (e.getType() == TableModelEvent.UPDATE) {
+//                    final int row = e.getFirstRow();
+//                    final int column = e.getColumn();
+//                    final Object value = ((SampleTableModel) e.getSource()).getValueAt(row, column);
+//
+//                    Platform.runLater(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            XYChart.Series<String, Number> s = (XYChart.Series<String, Number>) tempChart.getData().get(row);
+//                            BarChart.Data data = s.getData().get(column);
+//                            data.setYValue(value);
+//                        }
+//                    });
+//                }
+//            }
+//        });
+//        return tempChart;
+//    }
+//
+//    private static class DecimalFormatRenderer extends DefaultTableCellRenderer {
+//
+//        private static final DecimalFormat formatter = new DecimalFormat("#.0");
+//
+//        @Override
+//        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+//            value = formatter.format((Number) value);
+//            return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+//        }
+//    }
+//
+//    /**
+//     * This method is called from within the constructor to initialize the form.
+//     * WARNING: Do NOT modify this code. The content of this method is always
+//     * regenerated by the Form Editor.
+//     */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -183,25 +183,25 @@ public final class ProjectStatsTopComponent extends TopComponent {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
-    @Override
-    public void componentOpened() {
-        // TODO add custom code on component opening
-    }
-
-    @Override
-    public void componentClosed() {
-        // TODO add custom code on component closing
-    }
-
-    void writeProperties(java.util.Properties p) {
-        // better to version settings since initial version as advocated at
-        // http://wiki.apidesign.org/wiki/PropertyFiles
-        p.setProperty("version", "1.0");
-        // TODO store your settings
-    }
-
-    void readProperties(java.util.Properties p) {
-        String version = p.getProperty("version");
-        // TODO read your settings according to their version
-    }
+//    @Override
+//    public void componentOpened() {
+//        // TODO add custom code on component opening
+//    }
+//
+//    @Override
+//    public void componentClosed() {
+//        // TODO add custom code on component closing
+//    }
+//
+//    void writeProperties(java.util.Properties p) {
+//        // better to version settings since initial version as advocated at
+//        // http://wiki.apidesign.org/wiki/PropertyFiles
+//        p.setProperty("version", "1.0");
+//        // TODO store your settings
+//    }
+//
+//    void readProperties(java.util.Properties p) {
+//        String version = p.getProperty("version");
+//        // TODO read your settings according to their version
+//    }
 }
