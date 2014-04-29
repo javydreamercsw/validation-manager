0f7e2a565bf2552af44351c7d8dc9eb1b1344376
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2014-04-29 14:02:01 -0500

    Only have the latest version of requirements.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/RequirementChildFactory.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/RequirementChildFactory.java
@@ -4,7 +4,9 @@ import com.validation.manager.core.DataBaseManager;
 import com.validation.manager.core.db.Requirement;
 import com.validation.manager.core.db.RequirementSpecNode;
 import com.validation.manager.core.db.controller.RequirementSpecNodeJpaController;
+import com.validation.manager.core.server.core.RequirementServer;
 import java.beans.IntrospectionException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -39,16 +41,33 @@ public class RequirementChildFactory extends AbstractChildFactory
 
     @Override
     protected boolean createKeys(List<Object> toPopulate) {
+        List<Requirement> toAdd = new ArrayList<>();
         for (Requirement req : node.getRequirementList()) {
             //TODO: Filter out status ids
 //            if (!new ArrayList<>(Arrays.asList(ids)).contains(req.getRequirementStatusId().getId())) {
             if (req.getRequirementStatusId() != null
                     && req.getRequirementStatusId().getId() == 2) {
-                toPopulate.add(req);
+                boolean found = false;
+                for (Requirement in : toAdd) {
+                    if (in.getUniqueId().trim().equals(req.getUniqueId().trim())) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    //Only add the latest version
+                    RequirementServer rs = new RequirementServer(req);
+                    Requirement max = Collections.max(rs.getVersions(), null);
+                    toAdd.add(max);
+                }
             } else if (req.getRequirementStatusId() == null) {
                 LOG.log(Level.WARNING,
                         "Invalid Requirement without status: {0}", req);
             }
+        }
+        toPopulate.addAll(toAdd);
+        for (RequirementSpecNode req : node.getRequirementSpecNodeList()) {
+            toPopulate.add(req);
         }
         Collections.sort(toPopulate, new Comparator<Object>() {
 
@@ -59,9 +78,6 @@ public class RequirementChildFactory extends AbstractChildFactory
                         .compareToIgnoreCase(((Requirement) o2).getUniqueId());
             }
         });
-        for (RequirementSpecNode req : node.getRequirementSpecNodeList()) {
-            toPopulate.add(req);
-        }
         return true;
     }
 
