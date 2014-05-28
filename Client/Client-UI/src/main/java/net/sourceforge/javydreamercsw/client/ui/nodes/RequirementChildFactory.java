29ad5ecee2d81893370baf83dd93c78c65a0b20e
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2014-05-28 09:16:27 -0500

    Asynchronous loading of Requirements.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/RequirementChildFactory.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/RequirementChildFactory.java
@@ -6,7 +6,6 @@ import com.validation.manager.core.db.RequirementSpecNode;
 import com.validation.manager.core.db.controller.RequirementSpecNodeJpaController;
 import com.validation.manager.core.server.core.RequirementServer;
 import java.beans.IntrospectionException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -26,7 +25,8 @@ public class RequirementChildFactory extends AbstractChildFactory
         implements RequirementStatusFilterChangeListener {
 
     private RequirementSpecNode node;
-    private Integer[] ids = new Integer[0];
+//    private Integer[] ids = new Integer[0];
+    private Populator pop = null;
     private static final Logger LOG
             = Logger.getLogger(RequirementChildFactory.class.getSimpleName());
 
@@ -41,44 +41,80 @@ public class RequirementChildFactory extends AbstractChildFactory
 
     @Override
     protected boolean createKeys(List<Object> toPopulate) {
-        List<Requirement> toAdd = new ArrayList<>();
-        for (Requirement req : node.getRequirementList()) {
-            //TODO: Filter out status ids
+        if (pop == null) {
+            pop = new Populator(toPopulate);
+            pop.start();
+        }
+        boolean done = pop.done();
+        if (done && pop != null) {
+            pop = null;
+        }
+        return done;
+    }
+
+    private class Populator extends Thread {
+
+        boolean done = false;
+        private final List<Object> toPopulate;
+
+        public Populator(List<Object> toPopulate) {
+            super("Child Factory " + node.getName());
+            this.toPopulate = toPopulate;
+        }
+
+        @Override
+        public void run() {
+            Comparator<Object> comparator = new Comparator<Object>() {
+
+                @Override
+                public int compare(Object o1, Object o2) {
+                    //Sort them by unique id
+                    return ((Requirement) o1).getUniqueId()
+                            .compareToIgnoreCase(((Requirement) o2).getUniqueId());
+                }
+            };
+            int count = 0;
+            for (Requirement req : node.getRequirementList()) {
+                //TODO: Filter out status ids
 //            if (!new ArrayList<>(Arrays.asList(ids)).contains(req.getRequirementStatusId().getId())) {
-            if (req.getRequirementStatusId() != null
-                    && req.getRequirementStatusId().getId() == 2) {
-                boolean found = false;
-                for (Requirement in : toAdd) {
-                    if (in.getUniqueId().trim().equals(req.getUniqueId().trim())) {
-                        found = true;
-                        break;
+                if (req.getRequirementStatusId() != null
+                        && req.getRequirementStatusId().getId() == 2) {
+                    boolean found = false;
+                    for (Object obj : toPopulate) {
+                        if (obj instanceof Requirement) {
+                            Requirement in = (Requirement) obj;
+                            if (in.getUniqueId().trim().equals(req.getUniqueId().trim())) {
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!found) {
+                        //Only add the latest version
+                        RequirementServer rs = new RequirementServer(req);
+                        Requirement max = Collections.max(rs.getVersions(), null);
+                        toPopulate.add(max);
+                        count++;
+                        Collections.sort(toPopulate, comparator);
+                        if (count >= 10) {
+                            break;
+                        }
+                    } else if (req.getRequirementStatusId() == null) {
+                        LOG.log(Level.WARNING,
+                                "Invalid Requirement without status: {0}", req);
                     }
                 }
-                if (!found) {
-                    //Only add the latest version
-                    RequirementServer rs = new RequirementServer(req);
-                    Requirement max = Collections.max(rs.getVersions(), null);
-                    toAdd.add(max);
+                for (RequirementSpecNode rsn : node.getRequirementSpecNodeList()) {
+                    toPopulate.add(rsn);
                 }
-            } else if (req.getRequirementStatusId() == null) {
-                LOG.log(Level.WARNING,
-                        "Invalid Requirement without status: {0}", req);
+                Collections.sort(toPopulate, comparator);
+                done = true;
             }
         }
-        toPopulate.addAll(toAdd);
-        for (RequirementSpecNode req : node.getRequirementSpecNodeList()) {
-            toPopulate.add(req);
-        }
-        Collections.sort(toPopulate, new Comparator<Object>() {
 
-            @Override
-            public int compare(Object o1, Object o2) {
-                //Sort them by unique id
-                return ((Requirement) o1).getUniqueId()
-                        .compareToIgnoreCase(((Requirement) o2).getUniqueId());
-            }
-        });
-        return true;
+        public boolean done() {
+            return done;
+        }
     }
 
     @Override
@@ -115,7 +151,7 @@ public class RequirementChildFactory extends AbstractChildFactory
 
     @Override
     public void filterChange(Integer[] ids) {
-        this.ids = ids;
+//        this.ids = ids;
         refresh();
     }
 }
