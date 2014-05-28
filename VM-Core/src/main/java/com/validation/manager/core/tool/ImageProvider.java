e25b80b7c82f963fd70f28c4c920c8e6c4a91774
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2014-05-28 09:21:38 -0500

    New tools.

---
+++ VM-Core/src/main/java/com/validation/manager/core/tool/ImageProvider.java
@@ -0,0 +1,27 @@
+package com.validation.manager.core.tool;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public interface ImageProvider<T> {
+
+    /**
+     * Provide icons for entities.
+     *
+     * @param e Entity to get the icon for.
+     * @param type Icon type
+     * @return Icon or null if none found.
+     */
+    public BufferedImage getIcon(T e, int type) throws IOException;
+    
+    /**
+     * Is this entity supported
+     * @param e Object to get the icon for.
+     * @return true if supported
+     */
+    public boolean supported(Object e);
+}
