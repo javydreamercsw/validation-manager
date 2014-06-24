370395ca40fc29f6e8d0a0b0722392973aef0eb7
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2014-06-24 10:45:58 -0500

    Only show the latest version of the requirements.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/StepChildFactory.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/nodes/StepChildFactory.java
@@ -4,8 +4,10 @@ import com.validation.manager.core.DataBaseManager;
 import com.validation.manager.core.db.Requirement;
 import com.validation.manager.core.db.Step;
 import com.validation.manager.core.db.controller.StepJpaController;
+import com.validation.manager.core.server.core.RequirementServer;
 import java.beans.IntrospectionException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.openide.nodes.Node;
 import org.openide.util.Exceptions;
@@ -24,35 +26,14 @@ class StepChildFactory extends AbstractChildFactory {
 
     @Override
     protected boolean createKeys(List<Object> toPopulate) {
-        List<Requirement> finalList = new ArrayList<>();
-        List<Requirement> toAdd = new ArrayList<>();
-        List<Requirement> toRemove = new ArrayList<>();
+        List<String> processed = new ArrayList<>();
         for (Requirement r : step.getRequirementList()) {
-            boolean found = false;
-            for (Requirement in : finalList) {
-                if (r.getUniqueId().equals(in.getUniqueId())) {
-                    //They have the same Unique ID, so they are versions of the same requirement
-                    if (in.compareTo(r) < 0) {
-                        //The one in is older. Remove it and replace with the new one
-                        toRemove.add(in);
-                        toAdd.add(r);
-                        found = true;
-                    } else {
-                        //The one in is either the same or greater, just keep it
-                    }
-                }
-            }
-            if (!found) {
-                finalList.add(r);
+            if (!processed.contains(r.getUniqueId().trim())) {
+                processed.add(r.getUniqueId().trim());
+                RequirementServer rs = new RequirementServer(r);
+                toPopulate.add(Collections.max(rs.getVersions(), null));
             }
         }
-        for (Requirement add : toAdd) {
-            finalList.add(add);
-        }
-        for (Requirement remove : toRemove) {
-            finalList.remove(remove);
-        }
-        toPopulate.addAll(finalList);
         return true;
     }
 
