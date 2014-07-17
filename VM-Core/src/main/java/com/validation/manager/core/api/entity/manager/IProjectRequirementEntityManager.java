7b0d24a683e8fba5c51e5648a983bb10f87760e7
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2014-07-17 12:03:50 -0500

    Refactored new interface.

---
+++ VM-Core/src/main/java/com/validation/manager/core/api/entity/manager/IProjectRequirementEntityManager.java
@@ -0,0 +1,18 @@
+package com.validation.manager.core.api.entity.manager;
+
+import com.validation.manager.core.db.Project;
+import com.validation.manager.core.db.Requirement;
+import java.util.Collection;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public interface IProjectRequirementEntityManager {
+    /**
+     * Get requirements for specific project.
+     * @param p Project to look requirement from
+     * @return List of requirements.
+     */
+    public Collection<Requirement> getEntities(Project p);
+}
