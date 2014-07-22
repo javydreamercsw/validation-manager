5012b30f85d48a8221f096014ca07ed7674bfd81
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2014-07-22 09:38:45 -0500

    Change to make connection to database process start after selecting the database.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/database/ConnectAction.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/database/ConnectAction.java
@@ -5,17 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.netbeans.api.progress.ProgressHandle;
-import org.netbeans.api.progress.ProgressHandleFactory;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.util.Cancellable;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle.Messages;
-import org.openide.util.RequestProcessor;
-import org.openide.util.TaskListener;
 
 @ActionID(
         category = "Tools",
@@ -32,63 +27,11 @@ public final class ConnectAction implements ActionListener {
 
     private static final Logger LOG
             = Logger.getLogger(ConnectAction.class.getSimpleName());
-    private final RequestProcessor RP
-            = new RequestProcessor("Database Connector", 1, true);
-    private RequestProcessor.Task theTask = null;
-    private ProgressHandle ph;
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ph = ProgressHandleFactory.createHandle("Database Connector",
-                        new Cancellable() {
-
-                            @Override
-                            public boolean cancel() {
-                                return handleCancel();
-                            }
-                        });
-                Runnable runnable = new Runnable() {
-
-                    @Override
-                    public void run() {
-                        //Connect to database
-                        DataBaseTool.connect();
-                    }
-                };
-                theTask = RP.create(runnable); //the task is not started yet
-
-                theTask.addTaskListener(new TaskListener() {
-                    public void taskFinished(RequestProcessor.Task task) {
-                        ph.finish();
-                        LOG.log(Level.FINE,
-                                "Connecting to Database done!");
-                    }
-
-                    @Override
-                    public void taskFinished(org.openide.util.Task task) {
-                        ph.finish();
-                        LOG.log(Level.FINE,
-                                "Connecting to Database done!");
-                    }
-                });
-                //start the progresshandle the progress UI will show 500s after
-                ph.start();
-
-                //this actually start the task
-                theTask.schedule(0);
-            }
-
-            private boolean handleCancel() {
-                LOG.info("handleCancel");
-                if (null == theTask) {
-                    return false;
-                }
-                return theTask.cancel();
-            }
-        });
+        //Connect to database
+        DataBaseTool.connect();
         //Initialize any EntityManager that needs it
         for (VMEntityManager m : Lookup.getDefault().lookupAll(VMEntityManager.class)) {
             LOG.log(Level.FINE, "{0} registered!",
--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/database/DataBaseTool.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/database/DataBaseTool.java
@@ -4,10 +4,17 @@ import net.sourceforge.javydreamercsw.client.ui.components.project.explorer.Proj
 import com.validation.manager.core.DataBaseManager;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import org.netbeans.api.db.explorer.DatabaseConnection;
+import org.netbeans.api.progress.ProgressHandle;
+import org.netbeans.api.progress.ProgressHandleFactory;
+import org.openide.util.Cancellable;
 import org.openide.util.Exceptions;
+import org.openide.util.RequestProcessor;
+import org.openide.util.TaskListener;
 
 /**
  *
@@ -16,6 +23,12 @@ import org.openide.util.Exceptions;
 public class DataBaseTool {
 
     private static EntityManagerFactory emf;
+    private static final RequestProcessor RP
+            = new RequestProcessor("Database Connector", 1, true);
+    private static RequestProcessor.Task theTask = null;
+    private static ProgressHandle ph;
+    private static final Logger LOG
+            = Logger.getLogger(DataBaseTool.class.getSimpleName());
 
     public static void connect() {
         final DatabaseSelection dialog
@@ -28,23 +41,73 @@ public class DataBaseTool {
                 Exceptions.printStackTrace(ex);
             }
         }
-        DatabaseConnection conn = ProjectExplorerComponent.getConnection();
-        if (conn != null) {
-            Map addedOrOverridenProperties = new HashMap();
-            addedOrOverridenProperties.put("javax.persistence.jdbc.url",
-                    conn.getDatabaseURL());
-            addedOrOverridenProperties.put("javax.persistence.jdbc.password",
-                    conn.getPassword());
-            addedOrOverridenProperties.put("javax.persistence.jdbc.driver",
-                    conn.getDriverClass());
-            addedOrOverridenProperties.put("javax.persistence.jdbc.user",
-                    conn.getUser());
-            emf = Persistence.createEntityManagerFactory(
-                    DataBaseManager.getPersistenceUnitName(),
-                    addedOrOverridenProperties);
-            DataBaseManager.setEntityManagerFactory(getEmf());
-            ProjectExplorerComponent.refresh();
-        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ph = ProgressHandleFactory.createHandle(
+                        "Connecting to Database, please wait...",
+                        new Cancellable() {
+
+                            @Override
+                            public boolean cancel() {
+                                return handleCancel();
+                            }
+                        });
+                Runnable runnable = new Runnable() {
+
+                    @Override
+                    public void run() {
+                        //Connect to database
+                        DatabaseConnection conn = ProjectExplorerComponent.getConnection();
+                        if (conn != null) {
+                            Map addedOrOverridenProperties = new HashMap();
+                            addedOrOverridenProperties.put("javax.persistence.jdbc.url",
+                                    conn.getDatabaseURL());
+                            addedOrOverridenProperties.put("javax.persistence.jdbc.password",
+                                    conn.getPassword());
+                            addedOrOverridenProperties.put("javax.persistence.jdbc.driver",
+                                    conn.getDriverClass());
+                            addedOrOverridenProperties.put("javax.persistence.jdbc.user",
+                                    conn.getUser());
+                            emf = Persistence.createEntityManagerFactory(
+                                    DataBaseManager.getPersistenceUnitName(),
+                                    addedOrOverridenProperties);
+                            DataBaseManager.setEntityManagerFactory(getEmf());
+                            ProjectExplorerComponent.refresh();
+                        }
+                    }
+                };
+                theTask = RP.create(runnable); //the task is not started yet
+
+                theTask.addTaskListener(new TaskListener() {
+                    public void taskFinished(RequestProcessor.Task task) {
+                        ph.finish();
+                        LOG.log(Level.FINE,
+                                "Connecting to Database done!");
+                    }
+
+                    @Override
+                    public void taskFinished(org.openide.util.Task task) {
+                        ph.finish();
+                        LOG.log(Level.FINE,
+                                "Connecting to Database done!");
+                    }
+                });
+                //start the progresshandle the progress UI will show 500s after
+                ph.start();
+
+                //this actually start the task
+                theTask.schedule(0);
+            }
+
+            private boolean handleCancel() {
+                LOG.info("handleCancel");
+                if (null == theTask) {
+                    return false;
+                }
+                return theTask.cancel();
+            }
+        });
     }
 
     /**
