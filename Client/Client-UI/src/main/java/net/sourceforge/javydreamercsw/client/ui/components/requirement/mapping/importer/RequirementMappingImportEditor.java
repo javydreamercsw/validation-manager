bc04c41183c6e8b1feef8d829af5a8cb6a8c1b9d
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2015-03-06 14:36:40 -0600

    Add serial version ID.

--- Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/requirement/mapping/importer/RequirementMappingImportEditor.java
+++ Client/Client-UI/src/main/java/net/sourceforge/javydreamercsw/client/ui/components/requirement/mapping/importer/RequirementMappingImportEditor.java
@@ -3,6 +3,7 @@ package net.sourceforge.javydreamercsw.client.ui.components.requirement.mapping.
 import net.sourceforge.javydreamercsw.client.ui.components.ImportCustomEditor;
 
 public class RequirementMappingImportEditor extends ImportCustomEditor {
+    private static final long serialVersionUID = 6981691671253477164L;
 
     @Override
     public void init() {
