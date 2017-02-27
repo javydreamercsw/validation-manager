0da8c42e859f3ce7f3b08a670b521c21d3239b06
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2017-02-27 10:32:32 -0600

    Formatting.

--- VM-Core/src/main/java/com/validation/manager/core/server/core/TestPlanServer.java
+++ VM-Core/src/main/java/com/validation/manager/core/server/core/TestPlanServer.java
@@ -28,20 +28,24 @@ public final class TestPlanServer extends TestPlan implements EntityServer<TestP
         update(TestPlanServer.this, temp);
     }
 
-    public TestPlanServer(TestProject testProject, boolean active, boolean isOpen) {
+    public TestPlanServer(TestProject testProject, boolean active,
+            boolean isOpen) {
         super(testProject, active, isOpen);
         setTestProject(testProject);
     }
 
     @Override
-    public int write2DB() throws IllegalOrphanException, NonexistentEntityException, Exception {
-        TestPlanJpaController controller = new TestPlanJpaController(getEntityManagerFactory());
+    public int write2DB() throws IllegalOrphanException,
+            NonexistentEntityException, Exception {
+        TestPlanJpaController controller
+                = new TestPlanJpaController(getEntityManagerFactory());
         if (getTestPlanPK().getId() > 0) {
             TestPlan temp = controller.findTestPlan(getTestPlanPK());
             update(temp, this);
             controller.edit(temp);
         } else {
-            TestPlan temp = new TestPlan(getTestProject(), getActive(), getIsOpen());
+            TestPlan temp = new TestPlan(getTestProject(), getActive(),
+                    getIsOpen());
             update(temp, this);
             controller.create(temp);
             setTestPlanPK(temp.getTestPlanPK());
@@ -51,11 +55,9 @@ public final class TestPlanServer extends TestPlan implements EntityServer<TestP
 
     public static boolean deleteTestPlan(TestPlan tp) {
         try {
-            new TestPlanJpaController(getEntityManagerFactory()).destroy(tp.getTestPlanPK());
-        } catch (IllegalOrphanException ex) {
-            getLogger(TestPlanServer.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
-        } catch (NonexistentEntityException ex) {
+            new TestPlanJpaController(getEntityManagerFactory())
+                    .destroy(tp.getTestPlanPK());
+        } catch (IllegalOrphanException | NonexistentEntityException ex) {
             getLogger(TestPlanServer.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
@@ -65,18 +67,23 @@ public final class TestPlanServer extends TestPlan implements EntityServer<TestP
     public boolean addTest(Test test) {
         try {
             TestPlanHasTest tpht = new TestPlanHasTest(
-                    new TestPlanHasTestPK(getTestPlanPK().getId(), getTestPlanPK().getTestProjectId(), test.getId()),
+                    new TestPlanHasTestPK(getTestPlanPK().getId(),
+                            getTestPlanPK().getTestProjectId(), test.getId()),
                     new Date(), 1);
             tpht.setTest(test);
-            tpht.setTestPlan(new TestPlanJpaController(getEntityManagerFactory()).findTestPlan(getTestPlanPK()));
-            new TestPlanHasTestJpaController(getEntityManagerFactory()).create(tpht);
+            tpht.setTestPlan(new TestPlanJpaController(getEntityManagerFactory())
+                    .findTestPlan(getTestPlanPK()));
+            new TestPlanHasTestJpaController(getEntityManagerFactory())
+                    .create(tpht);
             getTestPlanHasTestList().add(tpht);
             write2DB();
             return true;
         } catch (PreexistingEntityException ex) {
-            getLogger(TestPlanServer.class.getName()).log(Level.SEVERE, null, ex);
+            getLogger(TestPlanServer.class.getName()).log(Level.SEVERE,
+                    null, ex);
         } catch (Exception ex) {
-            getLogger(TestPlanServer.class.getName()).log(Level.SEVERE, null, ex);
+            getLogger(TestPlanServer.class.getName()).log(Level.SEVERE,
+                    null, ex);
         }
         return false;
     }
@@ -98,7 +105,7 @@ public final class TestPlanServer extends TestPlan implements EntityServer<TestP
         target.setUserTestPlanRoleList(source.getUserTestPlanRoleList());
         target.setTestPlanPK(source.getTestPlanPK());
     }
-    
+
     @Override
     public void update() {
         update(this, getEntity());
