60752cb0ce21b6d7b2fe20c61561ddda9f41627c
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2017-03-02 16:10:07 -0600

    Added missing documentation.

--- VM-Core/src/main/java/com/validation/manager/core/ImporterInterface.java
+++ VM-Core/src/main/java/com/validation/manager/core/ImporterInterface.java
@@ -5,6 +5,7 @@ import java.util.List;
 /**
  *
  * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ * @param <T>
  */
 public interface ImporterInterface<T> {
 
@@ -16,7 +17,20 @@ public interface ImporterInterface<T> {
      */
     List<T> importFile() throws VMException;
 
+    /**
+     * Import the file.
+     *
+     * @param header File has header row.
+     * @return List of imported entities
+     * @throws ImportException If something goes wrong in the importing process.
+     */
     List<T> importFile(boolean header) throws VMException;
 
+    /**
+     * Process the imported data.
+     *
+     * @return true if processed successfully, false otherwise.
+     * @throws VMException
+     */
     boolean processImport() throws VMException;
 }
