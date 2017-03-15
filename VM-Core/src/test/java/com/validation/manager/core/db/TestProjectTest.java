541bee355aa0e30134b50eeb8fc78f6f2d9de676
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-03-15 10:20:54 -0500

    Fix test case.

--- VM-Core/src/test/java/com/validation/manager/core/db/TestProjectTest.java
+++ VM-Core/src/test/java/com/validation/manager/core/db/TestProjectTest.java
@@ -1,19 +1,16 @@
 package com.validation.manager.core.db;
 
 import static com.validation.manager.core.DataBaseManager.getEntityManagerFactory;
-import static com.validation.manager.core.DataBaseManager.namedQuery;
 import com.validation.manager.core.db.controller.RoleJpaController;
-import com.validation.manager.core.db.controller.TestProjectJpaController;
 import com.validation.manager.core.db.controller.UserTestProjectRoleJpaController;
 import com.validation.manager.core.db.controller.VmUserJpaController;
-import com.validation.manager.core.db.controller.exceptions.IllegalOrphanException;
-import com.validation.manager.core.db.controller.exceptions.NonexistentEntityException;
 import com.validation.manager.core.db.controller.exceptions.PreexistingEntityException;
+import com.validation.manager.core.server.core.TestProjectServer;
 import com.validation.manager.test.AbstractVMTestCase;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-import static java.util.logging.Logger.getLogger;
+import java.util.logging.Logger;
 import org.junit.Test;
 
 /**
@@ -22,7 +19,9 @@ import org.junit.Test;
  */
 public class TestProjectTest extends AbstractVMTestCase {
 
-    private TestProject tp;
+    private TestProjectServer tp;
+    private static final Logger LOG
+            = Logger.getLogger(TestProjectTest.class.getSimpleName());
 
     @Override
     public void setUp() {
@@ -30,31 +29,8 @@ public class TestProjectTest extends AbstractVMTestCase {
             super.setUp();
             createTestUsers();
         } catch (Exception ex) {
-            getLogger(TestProjectTest.class.getSimpleName()).log(Level.SEVERE,
-                    null, ex);
+            LOG.log(Level.SEVERE, null, ex);
             fail();
-        }
-    }
-
-    @Override
-    public void tearDown() {
-        try {
-            deleteTestUsers();
-            if (tp != null && tp.getId() != null
-                    && new TestProjectJpaController(getEntityManagerFactory())
-                            .findTestProject(tp.getId()) != null) {
-                new TestProjectJpaController(getEntityManagerFactory())
-                        .destroy(tp.getId());
-            }
-        } catch (IllegalOrphanException | NonexistentEntityException ex) {
-            getLogger(TestProjectTest.class.getSimpleName()).log(Level.SEVERE,
-                    null, ex);
-        }
-        try {
-            super.tearDown();
-        } catch (Exception ex) {
-            getLogger(TestProjectTest.class.getName()).log(Level.SEVERE, null,
-                    ex);
         }
     }
 
@@ -62,28 +38,22 @@ public class TestProjectTest extends AbstractVMTestCase {
     @SuppressWarnings("unchecked")
     public void testCreateTestProject() {
         try {
-            getLogger(TestProjectTest.class.getSimpleName()).log(Level.INFO,
-                    "Create Test Project");
+            LOG.log(Level.INFO, "Create Test Project");
             parameters.clear();
             parameters.put("name", "Test Project");
-            tp = new TestProject("Test Project", true);
-            new TestProjectJpaController(getEntityManagerFactory()).create(tp);
-            tp = ((TestProject) namedQuery("TestProject.findByName",
-                    parameters).get(0));
-            namedQuery("TestProject.findByName", parameters);
+            tp = new TestProjectServer("Test Project", true);
+            tp.write2DB();
             assertTrue(tp.getId() >= 0);
             assignRolesForTestProject();
         } catch (Exception ex) {
-            getLogger(TestProjectTest.class.getSimpleName()).log(Level.SEVERE,
-                    null, ex);
+            LOG.log(Level.SEVERE, null, ex);
             fail();
         }
     }
 
     private void assignRolesForTestProject() {
         try {
-            getLogger(TestProjectTest.class.getSimpleName()).log(Level.INFO,
-                    "Assigning roles");
+            LOG.log(Level.INFO, "Assigning roles");
             ArrayList<UserTestProjectRole> roles = new ArrayList<>();
             //Designer role
             UserTestProjectRole temp = new UserTestProjectRole(tp.getId(),
@@ -91,7 +61,7 @@ public class TestProjectTest extends AbstractVMTestCase {
             temp.setVmUser(designer);
             temp.setRole(new RoleJpaController(getEntityManagerFactory())
                     .findRole(4));
-            temp.setTestProject(tp);
+            temp.setTestProject(tp.getEntity());
             new UserTestProjectRoleJpaController(getEntityManagerFactory())
                     .create(temp);
             roles.add(temp);
@@ -103,7 +73,7 @@ public class TestProjectTest extends AbstractVMTestCase {
             temp.setVmUser(tester);
             temp.setRole(new RoleJpaController(getEntityManagerFactory())
                     .findRole(7));
-            temp.setTestProject(tp);
+            temp.setTestProject(tp.getEntity());
             new UserTestProjectRoleJpaController(getEntityManagerFactory())
                     .create(temp);
             roles.add(temp);
@@ -115,19 +85,17 @@ public class TestProjectTest extends AbstractVMTestCase {
             temp.setVmUser(leader);
             temp.setRole(new RoleJpaController(getEntityManagerFactory())
                     .findRole(9));
-            temp.setTestProject(tp);
+            temp.setTestProject(tp.getEntity());
             new UserTestProjectRoleJpaController(getEntityManagerFactory())
                     .create(temp);
             roles.add(temp);
             leader.setUserTestProjectRoleList((List) roles);
             new VmUserJpaController(getEntityManagerFactory()).edit(leader);
         } catch (PreexistingEntityException ex) {
-            getLogger(TestProjectTest.class.getSimpleName()).log(Level.SEVERE,
-                    null, ex);
+            LOG.log(Level.SEVERE, null, ex);
             fail();
         } catch (Exception ex) {
-            getLogger(TestProjectTest.class.getSimpleName()).log(Level.SEVERE,
-                    null, ex);
+            LOG.log(Level.SEVERE, null, ex);
             fail();
         }
     }
