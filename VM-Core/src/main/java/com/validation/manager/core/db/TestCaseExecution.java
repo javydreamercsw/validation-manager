5ab5c642b02a108baa5f1391e0896342bab05489
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-03-29 11:22:37 -0500

    Add fields to Test Execution

--- VM-Core/src/main/java/com/validation/manager/core/db/TestCaseExecution.java
+++ VM-Core/src/main/java/com/validation/manager/core/db/TestCaseExecution.java
@@ -9,12 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import org.codehaus.jackson.annotate.JsonIgnore;
@@ -39,7 +41,8 @@ public class TestCaseExecution implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "TestCaseExecGen")
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "TestCaseExecGen")
     @TableGenerator(name = "TestCaseExecGen", table = "vm_id",
             pkColumnName = "table_name",
             valueColumnName = "last_id",
@@ -49,6 +52,18 @@ public class TestCaseExecution implements Serializable {
     @NotNull
     @Column(name = "id")
     private Integer id;
+    @NotNull
+    @Lob
+    @Size(max = 2147483647)
+    @Column(name = "scope")
+    private String scope;
+    @NotNull
+    @Column(name = "name")
+    private String name;
+    @Lob
+    @Size(max = 2147483647)
+    @Column(name = "conclusion")
+    private String conclusion;
 
     public TestCaseExecution() {
     }
@@ -92,5 +107,47 @@ public class TestCaseExecution implements Serializable {
 
     public void setExecutionStepList(List<ExecutionStep> executionStepList) {
         this.executionStepList = executionStepList;
+    }
+
+    /**
+     * @return the scope
+     */
+    public String getScope() {
+        return scope;
+    }
+
+    /**
+     * @param scope the scope to set
+     */
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the conclusion
+     */
+    public String getConclusion() {
+        return conclusion;
+    }
+
+    /**
+     * @param conclusion the conclusion to set
+     */
+    public void setConclusion(String conclusion) {
+        this.conclusion = conclusion;
     }
 }
