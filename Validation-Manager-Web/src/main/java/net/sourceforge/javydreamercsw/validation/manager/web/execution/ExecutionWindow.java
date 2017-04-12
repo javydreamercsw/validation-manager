395074a6744295936aabd61b43cc434f750161f7
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-04-12 17:05:45 -0500

    Sort by test case and step number when presenting them.

--- Validation-Manager-Web/src/main/java/net/sourceforge/javydreamercsw/validation/manager/web/execution/ExecutionWindow.java
+++ Validation-Manager-Web/src/main/java/net/sourceforge/javydreamercsw/validation/manager/web/execution/ExecutionWindow.java
@@ -6,6 +6,7 @@ package net.sourceforge.javydreamercsw.validation.manager.web.execution;
 import com.vaadin.ui.HorizontalLayout;
 import com.validation.manager.core.server.core.TestCaseExecutionServer;
 import java.util.List;
+import java.util.TreeMap;
 import net.sourceforge.javydreamercsw.validation.manager.web.VMWindow;
 import net.sourceforge.javydreamercsw.validation.manager.web.ValidationManagerUI;
 import org.vaadin.teemu.wizards.Wizard;
@@ -53,11 +54,24 @@ public final class ExecutionWindow extends VMWindow {
     private void init(List<TestCaseExecutionServer> executions, int tcID) {
         HorizontalLayout layout = new HorizontalLayout();
         Wizard execution = new Wizard();
+        TreeMap<Integer, TreeMap<Integer, ExecutionWizardStep>> sorted = new TreeMap<>();
         executions.forEach((tce) -> {
             tce.getExecutionStepList().forEach(es -> {
-                if (tcID < 0 || es.getExecutionStepPK().getStepTestCaseId() == tcID) {
-                    execution.addStep(new ExecutionWizardStep(execution, ui, es));
+                if (tcID < 0
+                        || es.getExecutionStepPK().getStepTestCaseId() == tcID) {
+                    if (!sorted.containsKey(es.getExecutionStepPK().getStepTestCaseId())) {
+                        sorted.put(es.getExecutionStepPK().getStepTestCaseId(),
+                                new TreeMap<>());
+                    }
+                    sorted.get(es.getExecutionStepPK().getStepTestCaseId())
+                            .put(es.getStep().getStepSequence(),
+                                    new ExecutionWizardStep(execution, ui, es));
                 }
+            });
+        });
+        sorted.values().forEach(tm -> {
+            tm.values().forEach(ew -> {
+                execution.addStep(ew);
             });
         });
         execution.setDisplayedMaxTitles(3);
