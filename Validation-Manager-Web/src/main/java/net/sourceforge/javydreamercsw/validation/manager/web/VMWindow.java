2b30225ebb31076827a6ea9dc39c7acdebd3815f
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-04-19 13:11:31 -0500

    Use common small icon.

--- Validation-Manager-Web/src/main/java/net/sourceforge/javydreamercsw/validation/manager/web/VMWindow.java
+++ Validation-Manager-Web/src/main/java/net/sourceforge/javydreamercsw/validation/manager/web/VMWindow.java
@@ -1,38 +1,36 @@
 package net.sourceforge.javydreamercsw.validation.manager.web;
 
-import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.Window;
 
 /**
  *
  * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
  */
-public class VMWindow extends Window {
+public final class VMWindow extends Window {
 
     protected final ValidationManagerUI menu;
-    private final ThemeResource small = new ThemeResource("VMSmall.png");
 
     public VMWindow() {
         super();
         menu = null;
-        setIcon(small);
+        setIcon(ValidationManagerUI.SMALL_APP_ICON);
     }
 
     public VMWindow(String caption) {
         super(caption);
         menu = null;
-        setIcon(small);
+        setIcon(ValidationManagerUI.SMALL_APP_ICON);
     }
 
     public VMWindow(ValidationManagerUI menu, String caption) {
         super(caption);
         this.menu = menu;
-        setIcon(small);
+        setIcon(ValidationManagerUI.SMALL_APP_ICON);
     }
 
     public VMWindow(ValidationManagerUI menu) {
         super();
         this.menu = menu;
-        setIcon(small);
+        setIcon(ValidationManagerUI.SMALL_APP_ICON);
     }
 }
