a29020bfeca016b7a1ca23213a165fdc6dac0c3d
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-05-17 11:24:35 -0500

    Change version to 0.4.1-SNAPSHOT. Task #3 - Implement Trace Matrix

--- Validation-Manager-Web/src/main/java/net/sourceforge/javydreamercsw/validation/manager/web/traceability/TraceMatrix.java
+++ Validation-Manager-Web/src/main/java/net/sourceforge/javydreamercsw/validation/manager/web/traceability/TraceMatrix.java
@@ -5,6 +5,8 @@ import com.vaadin.server.Resource;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TreeTable;
 import com.validation.manager.core.VMUI;
+import com.validation.manager.core.db.ExecutionStep;
+import com.validation.manager.core.db.ExecutionStepHasIssue;
 import com.validation.manager.core.db.Project;
 import com.validation.manager.core.db.Requirement;
 import com.validation.manager.core.db.TestCase;
@@ -42,6 +44,9 @@ public class TraceMatrix extends TreeTable {
         addContainerProperty(ValidationManagerUI.getInstance()
                 .translate("general.result"),
                 Label.class, "");
+        addContainerProperty(ValidationManagerUI.getInstance()
+                .translate("general.issue"),
+                Label.class, "");
         Tool.extractRequirements(p).forEach((r) -> {
             if (r.getParentRequirementId() == null) {
                 addRequirement(r);
@@ -51,6 +56,7 @@ public class TraceMatrix extends TreeTable {
         for (Object item : getItemIds().toArray()) {
             expandItem(item);
         }
+        setChildrenAllowedAsNeeded();
         setSizeFull();
     }
 
@@ -65,12 +71,12 @@ public class TraceMatrix extends TreeTable {
 
     private void addRequirement(Requirement r) {
         addItem(new Object[]{r.getUniqueId(),
-            new Label(), new Label()}, r.getUniqueId());
+            new Label(), new Label(), new Label()}, r.getUniqueId());
         addTestCases(r);
         //Add children
         r.getRequirementList().forEach(child -> {
             addRequirement(child);
-            setParent(child.getUniqueId(), r.getUniqueId());
+            setParent(child.getUniqueId(), r.getUniqueId());;
         });
         setItemIcon(r.getUniqueId(), VMUI.REQUIREMENT_ICON);
     }
@@ -83,19 +89,17 @@ public class TraceMatrix extends TreeTable {
                 label.setIcon(VMUI.TEST_ICON);
                 if (!containsId("tc-" + tc.getId())) {
                     addItem(new Object[]{"",
-                        label, new Label()}, "tc-" + tc.getId());
+                        label, new Label(), new Label()}, "tc-" + tc.getId());
                     setParent("tc-" + tc.getId(), r.getUniqueId());
                 }
-                if (es.getResultId() != null
-                        && !containsId(es.getExecutionStepPK())) {
+                if (es.getResultId() != null /*&& !containsId(es.getExecutionStepPK())*/) {
                     String result = es.getResultId().getResultName();
                     Label resultLabel
                             = new Label(ValidationManagerUI.getInstance()
                                     .translate(result));
                     addItem(new Object[]{"",
                         new Label("Step #" + es.getStep().getStepSequence()),
-                        resultLabel}, es.getExecutionStepPK());
-                    setChildrenAllowed(es.getExecutionStepPK(), false);
+                        resultLabel, new Label()}, es.getExecutionStepPK());
                     setParent(es.getExecutionStepPK(), "tc-" + tc.getId());
                     //Completed. Now check result
                     Resource icon;
@@ -118,7 +122,27 @@ public class TraceMatrix extends TreeTable {
                     }
                     resultLabel.setIcon(icon);
                 }
+                addIssues(es);
             });
         });
+    }
+
+    private void setChildrenAllowedAsNeeded() {
+        getItemIds().forEach(item -> {
+            setChildrenAllowed(item,
+                    hasChildren(item));
+        });
+    }
+
+    private void addIssues(ExecutionStep es) {
+        for (ExecutionStepHasIssue eshi : es.getExecutionStepHasIssueList()) {
+            int issueNumber = eshi.getIssue().getIssuePK().getId();
+            Label label = new Label("Issue #" + issueNumber);
+            label.setIcon(VaadinIcons.BUG);
+            addItem(new Object[]{"",
+                new Label(), new Label(), label}, "issue-" + issueNumber);
+            setParent("issue-" + issueNumber, es.getExecutionStepPK());
+            setChildrenAllowed("issue-" + issueNumber, false);
+        }
     }
 }
